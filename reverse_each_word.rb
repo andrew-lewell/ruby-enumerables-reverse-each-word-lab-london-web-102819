4bde329d68701bb905619e2c55fbfb3dafff2eec
Andrew Lewell <andrew.lewell@gmail.com>
2019-10-15 12:35:40 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,17 +1,17 @@
 #def reverse_each_word(string)
-  array = []
-  array = string.split(" ")
-  array.each do |word| 
-    word.reverse!
-  end
-  final_array = array.join(" ")
-  final_array
-end
+#  array = []
+#  array = string.split(" ")
+#  array.each do |word| 
+#    word.reverse!
+#  end
+#  final_array = array.join(" ")
+#  final_array
+#end
 
 def reverse_each_word(string)
   array = []
   array = string.split(" ")
-  array.each do |word| 
+  array.collect do |word| 
     word.reverse!
   end
   final_array = array.join(" ")
